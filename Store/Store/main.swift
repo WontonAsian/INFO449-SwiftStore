ac3bad1f532294f6cd46ad68c712030557f8e9ca
Kevin Nguyen <97388692+WontonAsian@users.noreply.github.com>
2024-04-15 14:34:08 -0700

    pass all test cases

--- Store/Store/main.swift
+++ Store/Store/main.swift
@@ -7,18 +7,76 @@
 
 import Foundation
 
-protocol SKU {}
+protocol SKU {
+    var name: String { get }
+    func price() -> Int
+}
 
-class Item {}
+class Item: SKU {
+    var name: String
+    private var itemPriceEach: Int
 
-class Receipt {}
+    init(name: String, priceEach: Int) {
+        self.name = name
+        self.itemPriceEach = priceEach
+    }
 
-class Register {}
+    func price() -> Int {
+        return itemPriceEach
+    }
+}
+
+class Receipt {
+    private var itemsScanned: [SKU] = []
+
+    func addItem(_ item: SKU) {
+        itemsScanned.append(item)
+    }
+
+    func items() -> [SKU] {
+        return itemsScanned
+    }
+
+    func total() -> Int {
+        return items().reduce(0) { $0 + $1.price() }
+    }
+
+    func output() -> String {
+        let line = "------------------\n"
+        var receiptOutput = "Receipt:\n"
+        for item in itemsScanned {
+            receiptOutput += "\(item.name): $\(String(format: "%.2f", Double(item.price())/100.0))\n"
+        }
+        receiptOutput += line
+        receiptOutput += "TOTAL: $\(String(format: "%.2f", Double(total())/100.0))"
+        return receiptOutput
+    }
+}
+
+class Register {
+    private var currentReceipt = Receipt()
+
+    func scan(_ item: SKU) {
+        currentReceipt.addItem(item)
+    }
+
+    func subtotal() -> Int {
+        return currentReceipt.total()
+    }
+
+    func total() -> Receipt {
+        let finalReceipt = currentReceipt
+        currentReceipt = Receipt()
+        return finalReceipt
+    }
+}
 
 class Store {
     let version = "0.1"
+
     func helloWorld() -> String {
         return "Hello world"
     }
 }
+
 
